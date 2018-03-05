b20f0eec88e4f4dce204b540166b07904eadd050
pixochi <kozak.jakub55@gmail.com>
2018-03-05 12:22:33 +0100

    initial setup on Heroku

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import aframe from 'aframe';
+import { Scene } from 'aframe-react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Scene>
+
+      </Scene>
+    );
+  }
+}
+
+export default App;
---
+++ src/ducks/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+// import { reducer as elements } from './elements';
+// import { reducer as attributes } from './attributes';
+
+
+export default combineReducers({
+  sample: (state = {}, action) => state
+});
